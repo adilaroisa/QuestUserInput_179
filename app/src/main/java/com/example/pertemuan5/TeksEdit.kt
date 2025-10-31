a8e30eef56d65b4c3afd0107eb20f3981c0dd46f
adilaroisa <adilaroisa688@gmail.com>
2025-10-31 09:13:44 +0700

    menambahkan variabel nama untuk meningat masukan nilai dari keyboard

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -1,2 +1,8 @@
 package com.example.pertemuan5
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormDataDiri(modifier: Modifier = Modifier
+){
+    var textNama by remember {mutableStateOf("")}
