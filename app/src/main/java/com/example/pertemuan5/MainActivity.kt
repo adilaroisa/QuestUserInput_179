40a49867e14a72101822f291b29174cb46a227c0
adilaroisa <adilaroisa688@gmail.com>
2025-10-31 10:23:10 +0700

    mengganti pemanggilan fungsi

--- app/src/main/java/com/example/pertemuan5/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan5/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan5Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pertemuan5Theme {
-        Greeting("Android")
-    }
-}
