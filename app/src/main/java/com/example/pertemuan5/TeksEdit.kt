993cfc64e286ed865e6b789b10077069f85e5a52
adilaroisa <adilaroisa688@gmail.com>
2025-10-31 09:40:06 +0700

    membuat horizontal divider untuk pemisah

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -72,3 +72,12 @@ fun FormDataDiri(modifier: Modifier = Modifier
                 textAlamat = it
             }
         )
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(
+                    id = R.dimen.padding_medium
+                )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
