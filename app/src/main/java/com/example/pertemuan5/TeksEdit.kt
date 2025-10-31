c78d8a31004f9238bdd54cc34cfce4dc3de2d1b8
adilaroisa <adilaroisa688@gmail.com>
2025-10-31 09:24:01 +0700

    mengatur TextField

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -1,6 +1,11 @@
 package com.example.pertemuan5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier = Modifier
@@ -17,3 +22,17 @@ fun FormDataDiri(modifier: Modifier = Modifier
 
     // Daftar opsi untuk Radio Button Jenis Kelamin
     val gender: List<String> = listOf("Laki-laki","Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
