f3a5a85a0babf79b5a8f1bacb4a26cb62b3f1a7d
adilaroisa <adilaroisa688@gmail.com>
2025-10-31 09:19:11 +0700

    menambahkan komentar

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -5,12 +5,15 @@ import androidx.compose.runtime.Composable
 @Composable
 fun FormDataDiri(modifier: Modifier = Modifier
 ){
+    // variabel-variabel untuk mengingat nilai masukan dari keyboard
     var textNama by remember {mutableStateOf("")}
     var textAlamat by remember {mutableStateOf("")}
     var textJK by remember {mutableStateOf("")}
 
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
     var nama by remember {mutableStateOf("")}
     var alamat by remember {mutableStateOf("")}
     var jenis by remember {mutableStateOf("")}
 
+    // Daftar opsi untuk Radio Button Jenis Kelamin
     val gender: List<String> = listOf("Laki-laki","Perempuan")
