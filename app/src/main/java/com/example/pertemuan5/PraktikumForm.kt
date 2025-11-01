949886b9d8bfd80e499811eafd91d5f98ca5f1b8
adilaroisa <adilaroisa688@gmail.com>
2025-11-02 01:59:25 +0700

    menambahkan warna

---
+++ app/src/main/java/com/example/pertemuan5/PraktikumForm.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan5
+
+import com.example.pertemuan5.R
+
+
+private val PrimaryBlue = Color(0xFF64B5F6) // Biru untuk tombol
+private val HeaderBackground = Color(0xFFE3F2FD) // Biru untuk header
+private val White = Color.White
+private val LightGray = Color(0xFFD3D3D3)
+
+
